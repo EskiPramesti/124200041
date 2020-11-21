567ba167ab4c918e6533ff51e6cf25eb1cfa53cf
EskiPramesti <124200041@student.upnyk.ac.id>
2020-11-21 19:57:54 +0700

    bugfix test 5

--- Tugas5_Nestedif.cpp
+++ Tugas5_Nestedif.cpp
@@ -5,51 +5,46 @@
 #include <iomanip>
 using namespace std;
 
-int main ()
-
-{
+int main () {
 	
-	int pilih,n,j,i;
+	int pilih;
 
-	
 	cout << "Gambar Bangun Datar ==========="<<endl;
 	cout << "1. persegi"<<endl;
 	cout << "2. segitiga"<<endl;
 	cout << "Pilih : ";cin>>pilih;
 	
-	if (pilih == 1)
-	{
-		cout << "\n Panjang : ";cin>>n;
-		cout << "\n Lebar : ";cin>>j;
-	}
-		{if (n > 2)
-		{	for (int i=0; i<n; i++)
-		{	for (int j=0; j<n; j++)
-		{	if (( i==0)|| (i==n-1)||(j==0)|| (j==n-1))
-			{ cout << ("*");}
-		else if
-		{if (n%2==1)
-			{cout << (" "); } 
+	if (pilih == 1) {
+		int rows,columns,i,j;
+		cout<<"Enter the number of rows\n";
+		cin>>rows;//Takes input from user for rows
+		cout<<"Enter the number of columns\n";
+		cin>>columns;//Takes input from user for columns
+		
+		for (i=1; i<=rows; i++) {
+			for (j=1; j<=columns; j++){
+				if(i==1||i==rows||j==1||j==columns){
+					cout<<"*";
+				} else {
+					cout<<" ";
+				}
+			}
+			cout<<"\n";
 		}
-		else 
-		{ cout << (" "); }
 	}
-	{
-		cout <<"n"
-	}
-	}
-	 if (pilih == 2)
-	{
-		cout << "\n Tinggi : ";cin>>i;
-	}
-		scanf("%d",&n);
- for(i=n;i>=1;i--){
-  for(j=1;j<=i;j++)
-  {
-  printf("%d",j);
-  }
-  printf("\n");
- }
 	
+	if (pilih == 2) {
+		int rows;
 
+		cout << "Enter number of rows: ";
+		cin >> rows;
+
+		for(int i = 1; i <= rows; ++i){
+			for(int j = 1; j <= i; ++j){
+				cout << j << " ";
+			}
+			cout << "\n";
+		}
+	}
 }
+
